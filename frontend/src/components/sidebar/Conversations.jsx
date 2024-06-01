89634cf4c343ec96ae6675532f77ab9c170a274f
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-01 18:31:50 +0500

    zustand conversation selection added, and sidebar conversation selection logic is added

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations'
+import { getRandomEmoji } from '../../utils/emoji';
 
 const Conversations = () => {
+  const {loading,conversations} = useGetConversations();
+
   return (
     <div className='py-1 flex flex-col overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+      {conversations.map((conversation,idx)=>(
+        <Conversation key={conversation._id}
+         emoji={getRandomEmoji()}
+         lastIdx={idx === conversations.length - 1}
+         conversation={conversation}/>
+      ))}
+       {loading? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
