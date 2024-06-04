7a78ab00019635f94fbe9ed86b66aa6444a1a4a9
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-04 17:07:22 +0500

    updating Socketcontext

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://chatapp-demo-slfx.onrender.com'],
+        origin: ['http://localhost:3000'],
         methods: ["GET", "POST"]
     }
 });
